3c8e6a7ab30db566cffae3523c377afb51db47bc
mihail <mihail@site.com>
2022-03-22 22:28:04 +0300

    тестовый grpc запрос, передача результата в шаблон

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-	"html/template"
+	"context"
 	"fmt"
+	"grpc_test/pkg/api"
+	"html/template"
 	"net/http"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
+type Result struct {
+	R int32
+	E error
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	var x int32 = 2
+	var y int32 = 2
+	var res *api.AddResponse
+	var rs = Result{}
+
+	conn, err := grpc.Dial(":8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	tpl := template.Must(template.ParseFiles("cmd/webserver/templates/index.html"))
-	tpl.Execute(w, nil)
+	if err == nil {
+		c := api.NewAdderClient(conn)
+		res, _ = c.Add(context.Background(), &api.AddRequest{X: x, Y: y})
+		rs.R = res.GetResult()
+	} else {
+		rs.E = err
+	}
+	tpl.Execute(w, rs)
 }
 
 func main() {
 	mux := http.NewServeMux()
 
 	fs := http.FileServer(http.Dir("cmd/webserver/assets"))
-    mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
 	mux.HandleFunc("/", indexHandler)
 	fmt.Println("Listening port :3000")
