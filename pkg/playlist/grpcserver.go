4cfafa88403e30e69597cb812df748f8ddde1ca5
mihail <mihail@site.com>
2022-04-10 15:42:18 +0300

    проверка на существование значений призаполнении структуры

--- pkg/playlist/grpcserver.go
+++ pkg/playlist/grpcserver.go
@@ -17,82 +17,81 @@ type GRPCServer struct {
 
 func (s *GRPCServer) Get(ctx context.Context, req *api.GetRequest) (*api.GetResponse, error) {
 	grpsResponse := GetPlaylistItems(req.PlaylistId, req.PageToken)
-	pageInfo := &api.GetResponse_Pageinfo{
-		TotalResults: uint32(grpsResponse.PageInfo.TotalResults),
-		ResultsPerPage: uint32(grpsResponse.PageInfo.ResultsPerPage),
-	}
-	// var item *api.GetResponse_Items
-	items := []*api.GetResponse_Items{}
-	for _, value := range grpsResponse.Items {
-		// log.Println(value.Snippet.Title, value.Snippet.Thumbnails.Maxres)
-		thumbnails := &api.GetResponse_Thumbnails{}
-		if value.Snippet.Thumbnails.Default != nil {
-			thumbnails.Default = &api.GetResponse_Default{
-				Url: value.Snippet.Thumbnails.Default.Url,
-				Width: uint32(value.Snippet.Thumbnails.Default.Width),
-				Height: uint32(value.Snippet.Thumbnails.Default.Height),
-			}
-		}
-		if value.Snippet.Thumbnails.Medium != nil {
-			thumbnails.Medium = &api.GetResponse_Medium{
-				Url: value.Snippet.Thumbnails.Medium.Url,
-				Width: uint32(value.Snippet.Thumbnails.Medium.Width),
-				Height: uint32(value.Snippet.Thumbnails.Medium.Height),
-			}
-		}
-		if value.Snippet.Thumbnails.High != nil {
-			thumbnails.High = &api.GetResponse_High{
-				Url: value.Snippet.Thumbnails.High.Url,
-				Width: uint32(value.Snippet.Thumbnails.High.Width),
-				Height: uint32(value.Snippet.Thumbnails.High.Height),
-			}
-		}
-		if value.Snippet.Thumbnails.Standard != nil {
-			thumbnails.Standard = &api.GetResponse_Standard{
-				Url: value.Snippet.Thumbnails.Standard.Url,
-				Width: uint32(value.Snippet.Thumbnails.Standard.Width),
-				Height: uint32(value.Snippet.Thumbnails.Standard.Height),
-			}
-		}
-		if value.Snippet.Thumbnails.Maxres != nil {
-			thumbnails.Maxres = &api.GetResponse_Maxres{
-				Url:    value.Snippet.Thumbnails.Maxres.Url,
-				Width:  uint32(value.Snippet.Thumbnails.Maxres.Width),
-				Height: uint32(value.Snippet.Thumbnails.Maxres.Height),
-			}
-		}
-		resourceId := &api.GetResponse_Resourceid{
-			Kind:    value.Snippet.ResourceId.Kind,
-			VideoId: value.Snippet.ResourceId.VideoId,
-		}
-		snippet := &api.GetResponse_Snippet {
-			PublishedAt: value.Snippet.PublishedAt,
-			ChannelId: value.Snippet.ChannelId,
-			Title: value.Snippet.Title,
-			Description: value.Snippet.Description,
-			Thumbnails: thumbnails,
-			ChannelTitle: value.Snippet.ChannelTitle,
-			PlaylistId: value.Snippet.PlaylistId,
-			Position: uint32(value.Snippet.Position),
-			ResourceId: resourceId,
-			VideoOwnerChannelTitle: value.Snippet.VideoOwnerChannelTitle,
-			VideoOwnerChannelId: value.Snippet.VideoOwnerChannelId,
-		}
-		item := &api.GetResponse_Items {
-			Kind: value.Kind,
-			Etag: value.Etag,
-			Id: value.Id,
-			Snippet: snippet,
-		}
-		items = append(items, item)
-	}
 	response := &api.GetResponse{
 		Kind: grpsResponse.Kind,
 		Etag: grpsResponse.Etag,
 		NextPageToken: grpsResponse.NextPageToken,
-		Items: items,
+		Items: []*api.GetResponse_Items{},
 		PrevPageToken: grpsResponse.PrevPageToken,
-		PageInfo: pageInfo,
+	}
+	for _, value := range grpsResponse.Items {
+		item := &api.GetResponse_Items {
+			Kind: value.Kind,
+			Etag: value.Etag,
+			Id: value.Id,
+		}
+		if value.Snippet != nil {
+			item.Snippet = &api.GetResponse_Snippet {
+				PublishedAt: value.Snippet.PublishedAt,
+				ChannelId: value.Snippet.ChannelId,
+				Title: value.Snippet.Title,
+				Description: value.Snippet.Description,
+				Thumbnails: &api.GetResponse_Thumbnails{},
+				ChannelTitle: value.Snippet.ChannelTitle,
+				PlaylistId: value.Snippet.PlaylistId,
+				Position: uint32(value.Snippet.Position),
+				VideoOwnerChannelTitle: value.Snippet.VideoOwnerChannelTitle,
+				VideoOwnerChannelId: value.Snippet.VideoOwnerChannelId,
+			}
+			if value.Snippet.Thumbnails.Default != nil {
+				item.Snippet.Thumbnails.Default = &api.GetResponse_Default{
+					Url: value.Snippet.Thumbnails.Default.Url,
+					Width: uint32(value.Snippet.Thumbnails.Default.Width),
+					Height: uint32(value.Snippet.Thumbnails.Default.Height),
+				}
+			}
+			if value.Snippet.Thumbnails.Medium != nil {
+				item.Snippet.Thumbnails.Medium = &api.GetResponse_Medium{
+					Url: value.Snippet.Thumbnails.Medium.Url,
+					Width: uint32(value.Snippet.Thumbnails.Medium.Width),
+					Height: uint32(value.Snippet.Thumbnails.Medium.Height),
+				}
+			}
+			if value.Snippet.Thumbnails.High != nil {
+				item.Snippet.Thumbnails.High = &api.GetResponse_High{
+					Url: value.Snippet.Thumbnails.High.Url,
+					Width: uint32(value.Snippet.Thumbnails.High.Width),
+					Height: uint32(value.Snippet.Thumbnails.High.Height),
+				}
+			}
+			if value.Snippet.Thumbnails.Standard != nil {
+				item.Snippet.Thumbnails.Standard = &api.GetResponse_Standard{
+					Url: value.Snippet.Thumbnails.Standard.Url,
+					Width: uint32(value.Snippet.Thumbnails.Standard.Width),
+					Height: uint32(value.Snippet.Thumbnails.Standard.Height),
+				}
+			}
+			if value.Snippet.Thumbnails.Maxres != nil {
+				item.Snippet.Thumbnails.Maxres = &api.GetResponse_Maxres{
+					Url:    value.Snippet.Thumbnails.Maxres.Url,
+					Width:  uint32(value.Snippet.Thumbnails.Maxres.Width),
+					Height: uint32(value.Snippet.Thumbnails.Maxres.Height),
+				}
+			}
+			if value.Snippet.ResourceId != nil {
+				item.Snippet.ResourceId = &api.GetResponse_Resourceid{
+					Kind:    value.Snippet.ResourceId.Kind,
+					VideoId: value.Snippet.ResourceId.VideoId,
+				}
+			}
+		}
+		response.Items = append(response.Items, item)
+	}
+	if grpsResponse.PageInfo != nil {
+		response.PageInfo = &api.GetResponse_Pageinfo{
+			TotalResults: uint32(grpsResponse.PageInfo.TotalResults),
+			ResultsPerPage: uint32(grpsResponse.PageInfo.ResultsPerPage),
+		}
 	}
 	return response, nil
 }
